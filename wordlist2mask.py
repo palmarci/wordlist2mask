dd9c01ba9ae530a1cf1474c155a114f35122a37b
palmarci <palmarci@users.noreply.github.com>
2019-04-23 10:45:07 +0200

    Update wordlist2mask.py

--- wordlist2mask.py
+++ wordlist2mask.py
@@ -1,12 +1,10 @@
 import sys
+from collections import Counter
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
-if (len(sys.argv) < 3):
+if (len(sys.argv) < 2):
 	print("\npython wordlist to hashcat mask converter and sorter\n")
-	print("Usage: 	python wordlist2mask.py [wordlist file] [outfile]")
-	print("Example: python wordlist2mask.py ntlm-hashes.txt ntlm.hcmask\n")
+	print("Usage: 	python wordlist2mask.py [wordlist file]")
+	print("Example: python wordlist2mask.py ntlm-hashes.txt\n")
 	sys.exit(1)
 
 with open(str(sys.argv[1])) as f:
@@ -16,7 +14,7 @@ content = [x.strip() for x in content]
 masks = set()
 
 for x in content:
-	x = x.decode('utf-8', 'ignore')
+	#x = x.decode('utf-8', 'ignore')
 	try:
 		word = ""
 		for c in x:
@@ -33,8 +31,10 @@ for x in content:
 	except Exception:
 		pass
 
-masks = sorted(masks, key=len)
 
-with open(str(sys.argv[2]),'w') as f:
-	for i in masks:
-		f.write("%s\n" % i)
+
+masks = sorted(masks, key=Counter(masks).get, reverse=True)
+masks = list(masks)
+
+for i in masks:
+	print(i)
